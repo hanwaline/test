f2464464c6702fc029e7ca528bf91046b16c4cee
hanwaline <95526638+hanwaline@users.noreply.github.com>
2021-12-05 12:34:03 +0900

    Add files via upload

---
+++ js/issphone.js
@@ -0,0 +1,8 @@
+﻿var Ftsize =1;
+  if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
+    Ftsize = Ftsize + Vle;
+if(Vle == "0") Ftsize = 1;
+window.parent.document.body.style.zoom = Ftsize;
+  } else {
+    return false;
+  }
