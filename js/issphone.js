668a014cd19b57d4bdb78d8cfefeca3a363939dd
hanwaline <95526638+hanwaline@users.noreply.github.com>
2021-12-05 12:40:45 +0900

    Delete issphone.js

--- js/issphone.js
+++
@@ -1,9 +0,0 @@
-﻿var Ftsize =1;
-  if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
-	alert("phone mode");
-    Ftsize = Ftsize + Vle;
-if(Vle == "0") Ftsize = 2;
-window.parent.document.body.style.zoom = Ftsize;
-  } else {
-    alert("PC mode");
-  }
