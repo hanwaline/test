632d73611ccfa487cbc6c03686acce2132e2144f
hanwaline <95526638+hanwaline@users.noreply.github.com>
2021-12-05 18:20:38 +0900

    Delete homephone directory

--- homephone/js/issphone.js
+++
@@ -1,9 +0,0 @@
-﻿alert("hi");
-  if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
-    alert("phone mode");
-	loadCSS("css\phone.css");
-  } else {
-    alert("PC mode");
-    transform-origin: top left;
-    transform: scale(5); 
-  }
