417c6006f2075261bb26ea10c1f232e9b5bb1d93
hanwaline <95526638+hanwaline@users.noreply.github.com>
2021-12-05 12:35:46 +0900

    Update issphone.js

--- js/issphone.js
+++ js/issphone.js
@@ -1,7 +1,7 @@
 ﻿var Ftsize =1;
   if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
     Ftsize = Ftsize + Vle;
-if(Vle == "0") Ftsize = 1;
+if(Vle == "0") Ftsize = 2;
 window.parent.document.body.style.zoom = Ftsize;
   } else {
     return false;
