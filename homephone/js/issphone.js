a27aa607980720803e9846ef283b8494f90f3617
hanwaline <95526638+hanwaline@users.noreply.github.com>
2021-12-05 18:21:34 +0900

    Add files via upload

---
+++ homephone/js/issphone.js
@@ -0,0 +1,9 @@
+﻿alert("hi");
+  if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
+    alert("phone mode");
+	loadCSS("css\phone.css");
+  } else {
+    alert("PC mode");
+    transform-origin: top left;
+    transform: scale(5); 
+  }
